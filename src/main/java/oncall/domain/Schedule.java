ea217a887e30c05a07de1c103ef9e90050b4fa34
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-08 12:27:39 +0900

    feat : 날짜 저장 및 예외처리

---
+++ src/main/java/oncall/domain/Schedule.java
@@ -0,0 +1,30 @@
+package oncall.domain;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import oncall.constant.DayOfTheWeek;
+
+public class Schedule {
+    private final LocalDate date;
+
+    public Schedule(int month, String dayOfWeek) {
+        validateMonth(month);
+        LocalDate scheduleDate = LocalDate.of(2023, month, 1);
+        validateDayOfWeek(scheduleDate,dayOfWeek);
+        
+        this.date = scheduleDate;
+    }
+
+    private void validateMonth(int month) {
+        if(month < 1 || month > 12) {
+            throw new IllegalArgumentException("유효하지 않은 입력 값입니다. 다시 입력해 주세요. - 월");
+        }
+    }
+
+    private void validateDayOfWeek(LocalDate localDate, String input) {
+        DayOfTheWeek realDayOfWeek = DayOfTheWeek.find(localDate.getDayOfWeek());
+        if(!realDayOfWeek.getKorean().equals(input)) {
+            throw new IllegalArgumentException("유효하지 않은 입력 값입니다. 다시 입력해 주세요. - 요일");
+        }
+    }
+}
