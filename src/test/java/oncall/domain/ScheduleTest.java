295f8327296d8bd70051d3a786cf6f7d6a785e1d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-08 12:27:47 +0900

    test : 날짜  예외처리 테스트

---
+++ src/test/java/oncall/domain/ScheduleTest.java
@@ -0,0 +1,21 @@
+package oncall.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ScheduleTest {
+    @Test
+    void validateMonth(){
+        assertThatThrownBy(() -> new Schedule(13,"월"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void validateDayOfWeek(){
+        assertThatThrownBy(() -> new Schedule(5,"화"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
