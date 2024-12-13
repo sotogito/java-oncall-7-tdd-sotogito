1136fb024b3b2c9fef1d9713fc1b70322976ded7
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-13 14:30:30 +0900

    feat : Staffs 전체 관리 객체 생성

---
+++ src/main/java/oncall/domain/Staffs.java
@@ -0,0 +1,18 @@
+package oncall.domain;
+
+import java.util.EnumMap;
+import oncall.constants.WorkType;
+import oncall.domain.staff.OnCallStaff;
+
+/**
+ * 그냥 Map으로 생성하는게 맞는듯
+ */
+public class Staffs {
+    private final EnumMap<WorkType, OnCallStaff> staffs = new EnumMap<>(WorkType.class);
+
+    public Staffs(OnCallStaff weekday, OnCallStaff weekend) {
+        staffs.put(WorkType.WEEKDAY, weekday);
+        staffs.put(WorkType.WEEKEND, weekend);
+    }
+    
+}
