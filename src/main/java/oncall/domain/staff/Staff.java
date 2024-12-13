0b4521bb0c455e5400ed7e3bc680bdc6efe4641e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-12-13 14:29:53 +0900

    feat : Staff 객체 생성

---
+++ src/main/java/oncall/domain/staff/Staff.java
@@ -0,0 +1,47 @@
+package oncall.domain.staff;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import oncall.constants.WorkType;
+
+public class Staff implements Comparable<Staff> {
+    private final String name;
+    private final WorkType workType;
+    private LocalDate workDate;
+
+    public Staff(String name, WorkType workType) {
+        validateNameLength(name);
+        this.name = name;
+        this.workType = workType;
+    }
+
+
+    private void validateNameLength(String name) {
+        if (name.isEmpty() || name.length() > 5) {
+            throw new IllegalArgumentException("직원의 이름은 1~5글자로 입력해주세요");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Staff staff = (Staff) o;
+        return Objects.equals(name, staff.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public int compareTo(Staff o) {
+        return this.workDate.compareTo(o.workDate);
+    }
+
+}
